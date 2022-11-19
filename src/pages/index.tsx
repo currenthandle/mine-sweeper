4cff7f8edf6146f487f5dfea3ad45efb7704e1c3
Casey <leptone55@gmail.com>
2022-11-19 16:22:26 -0700

    status

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import { type NextPage } from 'next'
+import { useEffect, useReducer } from 'react'
 import createGameBoard from '../../utils/createGameBoard'
 import MineSweeper from '../components/MineSweeper'
 
 const Home: NextPage = () => {
-  const gameBoard = createGameBoard()
+  const [state, reducer] = useReducer()
+  useEffect(() => {
+    const { gameBoard, minePositions } = createGameBoard()
+  }, [])
   return (
     <div>
       <MineSweeper initalBoard={gameBoard}></MineSweeper>
