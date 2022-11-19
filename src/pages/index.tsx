c3c4d89058e20f326e1af736049334c1bb83f174
Casey <leptone55@gmail.com>
2022-11-19 12:46:13 -0700

    import json validate it

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import { type NextPage } from 'next'
+import createGameBoard from '../../utils/createGameBoard'
 import MineSweeper from '../components/MineSweeper'
 
 const Home: NextPage = () => {
+  const gameBoard = createGameBoard()
   return (
     <div>
       <MineSweeper></MineSweeper>
--- utils/createGameBoard.ts
+++ utils/createGameBoard.ts
@@ -1,2 +1,28 @@
 import { z } from 'zod'
-import grid from '../../public/grid.json'
+import data from '../public/grid.json'
+
+const jsonValidator = z.object({
+  data: z.array(z.array(z.number())),
+})
+const gridValidator = z.array(z.array(z.number()))
+
+// construct gameboard
+function constructBoard(grid) {
+  console.log('grid', grid)
+  console.log('Array.isArray(grid)', Array.isArray(grid))
+
+  grid.map((row, i) => {
+    row.map((col, j) => {
+      return {}
+    })
+  })
+}
+
+// count neighboring mines
+
+export default function createGameBoard() {
+  console.log('grid', data)
+  jsonValidator.parse(data)
+  const grid = gridValidator.parse(data.data)
+  const gameBoard = constructBoard(grid)
+}
