d3734a3b7fda9fb5a1bc1ec55b70b18574ece7c3
Casey <leptone55@gmail.com>
2022-11-19 16:53:31 -0700

    stat

--- src/components/GameBoardGrid.tsx
+++ src/components/GameBoardGrid.tsx
@@ -1,6 +1,15 @@
 import { Board } from '../../utils/validators'
+import React from 'react'
 
 const GameBoardGrid = ({ board }: { board: Board }) => {
-  return <div>GameBoardGrid</div>
+  return (
+    <div className='grid grid-cols-3'>
+      {board.map((row) => {
+        return row.map((cell) => {
+          return <div>Cell</div>
+        })
+      })}
+    </div>
+  )
 }
 export default GameBoardGrid
