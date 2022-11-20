9a26640e7f0649caba09b3dd0e922d8c8f1c6d4f
Casey <leptone55@gmail.com>
2022-11-19 22:20:08 -0600

    basic click

--- src/utils/reducer.ts
+++ src/utils/reducer.ts
@@ -5,8 +5,22 @@ const reducer = (state: State, action: Action) => {
     case 'clickCell': {
       console.log(' in click')
       const cell = action.payload
-      if (cell.flagged) {
+      console.log('cell', cell)
+      if (cell.flagged || cell.shown || !state.alive || !state.gameOn) {
         return state
+      }
+      if (cell.numNeighborMines === 0 && !cell.mine) {
+        console.log('0')
+      } else {
+        console.log('else')
+        // should have to do this
+        console.log('in else')
+        const newBoard = [...state.board]
+        newBoard[cell.position[0]][cell.position[1]].shown = true
+        return {
+          ...state,
+          board: newBoard,
+        }
       }
 
       return state
