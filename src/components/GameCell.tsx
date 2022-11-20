39a682fc9a3fcf0a0dc6af7cdd7d22379552e02a
Casey <leptone55@gmail.com>
2022-11-19 19:46:45 -0600

    styling bg

--- src/components/GameCell.tsx
+++ src/components/GameCell.tsx
@@ -2,8 +2,19 @@ import React from 'react'
 import type { Cell } from '../utils/validators'
 
 const GameCell = ({ cell }: { cell: Cell }): JSX.Element => {
+  const getBackgroundColor = (shown) => {
+    if (shown) {
+      return 'bg-gray-300'
+    } else {
+      return 'bg-gray-500'
+    }
+  }
   return (
-    <div className='border border-black w-10 h-10 items-center justify-center	grid'>
+    <div
+      className={`border border-black w-10 h-10 items-center justify-center	grid ${getBackgroundColor(
+        cell.shown
+      )}`}
+    >
       {cell.mine ? '*' : cell.numNeighborMines}
     </div>
   )
